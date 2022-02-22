6e4dfe1335757c75197cb05d1c8de0a8b8c3323d
zdraffko <mihov.dev@gmail.com>
2022-02-22 23:10:47 +0200

    Added a bootstrap logger in the Program.cd file to log any exceptions during the app's startup
    Enabled Serilog's selflog.
    
    Configured the elasticsearch sing to buffer log events in a file before sending them to elasticsearch to improve the resiliency against partial outages of the elasticsearch nodes.

--- src/Contexts/Identity/SuperTutor.Contexts.Identity.Startup/Program.cs
+++ src/Contexts/Identity/SuperTutor.Contexts.Identity.Startup/Program.cs
@@ -4,60 +4,89 @@ using MassTransit;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using Serilog.Debugging;
 using Serilog.Sinks.Elasticsearch;
 using SuperTutor.Contexts.Identity.Api;
 using SuperTutor.Contexts.Identity.Persistence;
 using SuperTutor.Contexts.Identity.Persistence.Entities;
 using SuperTutor.Contexts.Identity.Startup.Modules;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration)
-    => loggerConfiguration
-        .WriteTo.Console()
-        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(hostBuilderContext.Configuration["Elasticsearch:Url"]))
-        {
-            IndexFormat = $"supertutor-logs-identity-{DateTime.UtcNow:yyyy-MM}",
-            AutoRegisterTemplate = true,
-            DetectElasticsearchVersion = true,
-            TypeName = null,
-            BatchAction = ElasticOpType.Create,
-            ModifyConnectionSettings = connectionConfiguration => connectionConfiguration.BasicAuthentication(hostBuilderContext.Configuration["Elasticsearch:Username"], hostBuilderContext.Configuration["Elasticsearch:Password"]),
-        })
-        .ReadFrom.Configuration(hostBuilderContext.Configuration));
-
-// Add services to the container.
-
-builder.Services
-    .AddControllers()
-    .AddApplicationPart(typeof(IIdentityApiAssemblyMarker).Assembly)
-    .AddControllersAsServices();
-
-builder.Services.AddDbContext<IdentityDbContext>(options => options.UseSqlServer(builder.Configuration["Database:ConnectionString"]));
-
-builder.Services
-    .AddIdentity<User, IdentityRole<Guid>>()
-    .AddEntityFrameworkStores<IdentityDbContext>();
-
-builder.Services.AddMassTransitHostedService();
-
-builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder
-    => containerBuilder
-        .RegisterModule(new ApplicationModule())
-        .RegisterModule(new InfrastructureModule(builder.Configuration)));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseDeveloperExceptionPage();
+    var builder = WebApplication.CreateBuilder(args);
+
+    var selfLogFileWriter = TextWriter.Synchronized(File.CreateText("./logs/selflog"));
+
+    SelfLog.Enable(message =>
+    {
+        Console.WriteLine(message);
+
+        selfLogFileWriter.WriteLine(message);
+        selfLogFileWriter.Flush();
+    });
+
+    builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration)
+        => loggerConfiguration
+            .WriteTo.Console()
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(hostBuilderContext.Configuration["Elasticsearch:Url"]))
+            {
+                IndexFormat = $"supertutor-logs-identity-{DateTime.UtcNow:yyyy-MM}",
+                AutoRegisterTemplate = true,
+                DetectElasticsearchVersion = true,
+                TypeName = null,
+                BatchAction = ElasticOpType.Create,
+                ModifyConnectionSettings = connectionConfiguration => connectionConfiguration.BasicAuthentication(hostBuilderContext.Configuration["Elasticsearch:Username"], hostBuilderContext.Configuration["Elasticsearch:Password"]),
+                BufferBaseFilename = "./logs/buffers/elasticsearch"
+            })
+            .ReadFrom.Configuration(hostBuilderContext.Configuration));
+
+    // Add services to the container.
+
+    builder.Services
+        .AddControllers()
+        .AddApplicationPart(typeof(IIdentityApiAssemblyMarker).Assembly)
+        .AddControllersAsServices();
+
+    builder.Services.AddDbContext<IdentityDbContext>(options => options.UseSqlServer(builder.Configuration["Database:ConnectionString"]));
+
+    builder.Services
+        .AddIdentity<User, IdentityRole<Guid>>()
+        .AddEntityFrameworkStores<IdentityDbContext>();
+
+    builder.Services.AddMassTransitHostedService();
+
+    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+    builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder
+        => containerBuilder
+            .RegisterModule(new ApplicationModule())
+            .RegisterModule(new InfrastructureModule(builder.Configuration)));
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+
+    app.UseRouting();
+
+    app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+    app.Run();
 }
-
-app.UseRouting();
-
-app.UseEndpoints(endpoints => endpoints.MapControllers());
-
-app.Run();
+catch (Exception exception)
+{
+    Log.Fatal(exception, "An unhandled exception was thrown with message {ErrorMessage}", exception.Message);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
--- src/Contexts/Profiles/SuperTutor.Contexts.Profiles.Startup/Program.cs
+++ src/Contexts/Profiles/SuperTutor.Contexts.Profiles.Startup/Program.cs
@@ -3,58 +3,87 @@ using Autofac.Extensions.DependencyInjection;
 using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using Serilog.Debugging;
 using Serilog.Sinks.Elasticsearch;
 using SuperTutor.Contexts.Profiles.Api;
 using SuperTutor.Contexts.Profiles.Persistence.Contexts;
 using SuperTutor.Contexts.Profiles.Startup.Modules;
 using SuperTutor.SharedLibraries.BuildingBlocks.Domain.Utility.IdentifierConversion.JsonConversion;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration)
-    => loggerConfiguration
-        .WriteTo.Console()
-        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(hostBuilderContext.Configuration["Elasticsearch:Url"]))
-        {
-            IndexFormat = $"supertutor-logs-profiles-{DateTime.UtcNow:yyyy-MM}",
-            AutoRegisterTemplate = true,
-            DetectElasticsearchVersion = true,
-            TypeName = null,
-            BatchAction = ElasticOpType.Create,
-            ModifyConnectionSettings = connectionConfiguration => connectionConfiguration.BasicAuthentication(hostBuilderContext.Configuration["Elasticsearch:Username"], hostBuilderContext.Configuration["Elasticsearch:Password"]),
-        })
-        .ReadFrom.Configuration(hostBuilderContext.Configuration));
-
-// Add services to the container.
-
-builder.Services
-    .AddControllers()
-    .AddJsonOptions(jsonOptions => jsonOptions.JsonSerializerOptions.Converters.Add(new IdentifierJsonConverterFactory()))
-    .AddApplicationPart(typeof(IProfilesApiAssemblyMarker).Assembly)
-    .AddControllersAsServices();
-
-builder.Services.AddDbContext<ProfilesDbContext>(options => options.UseSqlServer(builder.Configuration["Database:ConnectionString"]));
-
-builder.Services.AddMassTransitHostedService();
-
-builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder
-    => containerBuilder
-        .RegisterModule(new ApplicationModule())
-        .RegisterModule(new InfrastructureModule(builder.Configuration))
-        .RegisterModule(new PersistenceModule()));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseDeveloperExceptionPage();
+    var builder = WebApplication.CreateBuilder(args);
+
+    var selfLogFileWriter = TextWriter.Synchronized(File.CreateText("./logs/selflog"));
+
+    SelfLog.Enable(message =>
+    {
+        Console.WriteLine(message);
+
+        selfLogFileWriter.WriteLine(message);
+        selfLogFileWriter.Flush();
+    });
+
+    builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration)
+        => loggerConfiguration
+            .WriteTo.Console()
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(hostBuilderContext.Configuration["Elasticsearch:Url"]))
+            {
+                IndexFormat = $"supertutor-logs-profiles-{DateTime.UtcNow:yyyy-MM}",
+                AutoRegisterTemplate = true,
+                DetectElasticsearchVersion = true,
+                TypeName = null,
+                BatchAction = ElasticOpType.Create,
+                ModifyConnectionSettings = connectionConfiguration => connectionConfiguration.BasicAuthentication(hostBuilderContext.Configuration["Elasticsearch:Username"], hostBuilderContext.Configuration["Elasticsearch:Password"]),
+                BufferBaseFilename = "./logs/buffers/elasticsearch"
+            })
+            .ReadFrom.Configuration(hostBuilderContext.Configuration));
+
+    // Add services to the container.
+
+    builder.Services
+        .AddControllers()
+        .AddJsonOptions(jsonOptions => jsonOptions.JsonSerializerOptions.Converters.Add(new IdentifierJsonConverterFactory()))
+        .AddApplicationPart(typeof(IProfilesApiAssemblyMarker).Assembly)
+        .AddControllersAsServices();
+
+    builder.Services.AddDbContext<ProfilesDbContext>(options => options.UseSqlServer(builder.Configuration["Database:ConnectionString"]));
+
+    builder.Services.AddMassTransitHostedService();
+
+    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+    builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder
+        => containerBuilder
+            .RegisterModule(new ApplicationModule())
+            .RegisterModule(new InfrastructureModule(builder.Configuration))
+            .RegisterModule(new PersistenceModule()));
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+
+    app.UseRouting();
+
+    app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+    app.Run();
 }
-
-app.UseRouting();
-
-app.UseEndpoints(endpoints => endpoints.MapControllers());
-
-app.Run();
+catch (Exception exception)
+{
+    Log.Fatal(exception, "An unhandled exception was thrown with message {ErrorMessage}", exception.Message);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
