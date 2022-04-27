5218672604e586b4c53b1b22323ef56f96430b1a
zdraffko <mihov.dev@gmail.com>
2022-04-27 13:19:28 +0300

    Added an aggregate root base class to be used with event sourcing.

---
+++ src/SharedLibraries/BuildingBlocks/SuperTutor.SharedLibraries.BuildingBlocks.Domain/Entities/Aggregates/AggregateRoot.cs
@@ -0,0 +1,28 @@
+﻿using SuperTutor.SharedLibraries.BuildingBlocks.Domain.Entities.Contracts;
+using SuperTutor.SharedLibraries.BuildingBlocks.Domain.Events;
+using SuperTutor.SharedLibraries.BuildingBlocks.Domain.ValueObjects.Identifiers;
+
+namespace SuperTutor.SharedLibraries.BuildingBlocks.Domain.Entities.Aggregates;
+
+public abstract class AggregateRoot<TIdentifier, TIdentifierValue> : Entity<TIdentifier, TIdentifierValue>, IAggregateRoot
+    where TIdentifier : Identifier<TIdentifierValue>
+    where TIdentifierValue : struct
+{
+    private readonly List<DomainEvent> domainEvents;
+
+    protected AggregateRoot(TIdentifier id) : base(id) => domainEvents = new List<DomainEvent>();
+
+    public IReadOnlyCollection<DomainEvent> DomainEvents => domainEvents;
+
+    protected void RaiseDomainEvent(DomainEvent domainEvent) => domainEvents.Add(domainEvent);
+
+    protected abstract void ApplyDomainEvent(DomainEvent domainEvent);
+
+    public void ApplyDomainEvents(IEnumerable<DomainEvent> domainEvents)
+    {
+        foreach (var domainEvent in domainEvents)
+        {
+            ApplyDomainEvent(domainEvent);
+        }
+    }
+}
---
+++ src/SharedLibraries/BuildingBlocks/SuperTutor.SharedLibraries.BuildingBlocks.Domain/Events/DomainEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace SuperTutor.SharedLibraries.BuildingBlocks.Domain.Events;
+
+public abstract class DomainEvent
+{
+    public DomainEvent()
+    {
+        Id = Guid.NewGuid();
+        OccurredOn = DateTime.UtcNow;
+    }
+
+    protected DomainEvent(Guid id, DateTime occurredOn)
+    {
+        Id = id;
+        OccurredOn = occurredOn;
+    }
+
+    public Guid Id { get; }
+
+    public DateTime OccurredOn { get; }
+}
