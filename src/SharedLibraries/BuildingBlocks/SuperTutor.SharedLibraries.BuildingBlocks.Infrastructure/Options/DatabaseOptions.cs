8ceeacfb01720042b2b5238a100efe91e3805b29
zdraffko <mihov.dev@gmail.com>
2022-04-27 18:40:05 +0300

    Removed the database options class as they are no longer used.

--- src/SharedLibraries/BuildingBlocks/SuperTutor.SharedLibraries.BuildingBlocks.Infrastructure/Options/DatabaseOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SuperTutor.SharedLibraries.BuildingBlocks.Infrastructure.Options;
-
-public class DatabaseOptions
-{
-    public const string OptionsSectionName = "Database";
-
-    public string ConnectionString { get; set; } = default!;
-}
