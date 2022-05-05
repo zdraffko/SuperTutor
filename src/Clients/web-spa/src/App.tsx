f5cdce2ccb94a0ab3715e10cc86623a6ecff061b
zdraffko <mihov.dev@gmail.com>
2022-05-05 20:40:52 +0300

    Added eslint and prettier.

--- src/Clients/web-spa/src/App.tsx
+++ src/Clients/web-spa/src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
@@ -8,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Dont Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
