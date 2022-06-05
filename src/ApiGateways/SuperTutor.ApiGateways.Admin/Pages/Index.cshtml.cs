e42b0f0bed3c350be137f6dd587ea79c4bb849bc
zdraffko <mihov.dev@gmail.com>
2022-06-05 21:53:47 +0300

    AdminApiGateway - Added functionality for approving a profile.

--- src/ApiGateways/SuperTutor.ApiGateways.Admin/Pages/Index.cshtml.cs
+++ src/ApiGateways/SuperTutor.ApiGateways.Admin/Pages/Index.cshtml.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
 using SuperTutor.ApiGateways.Admin.Models.Profiles;
 using SuperTutor.ApiGateways.Admin.Options;
+using System.Security.Claims;
 using System.Text.Json;
 
 namespace SuperTutor.ApiGateways.Admin.Pages;
@@ -9,11 +11,13 @@ public class IndexModel : PageModel
 {
     private static readonly HttpClient httpClient = new();
     private readonly string ProfilesApiUrl;
+    private readonly IHttpContextAccessor httpContextAccessor;
 
-    public IndexModel(IOptionsSnapshot<ApiUrlsOptions> apiUrlsOptions)
+    public IndexModel(IOptionsSnapshot<ApiUrlsOptions> apiUrlsOptions, IHttpContextAccessor httpContextAccessor)
     {
         ProfilesApiUrl = apiUrlsOptions.Value.Profiles;
         TutorProfiles = new List<TutorProfile>();
+        this.httpContextAccessor = httpContextAccessor;
     }
 
     public async Task OnGet()
@@ -26,4 +30,21 @@ public class IndexModel : PageModel
     }
 
     public IEnumerable<TutorProfile> TutorProfiles { get; set; }
+
+    public async Task<ActionResult> OnPostApproveTutorProfile(string tutorProfileId)
+    {
+        var cancellationToken = new CancellationTokenSource().Token;
+
+        var adminId = httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        var profilesRequest = new
+        {
+            TutorProfileId = tutorProfileId,
+            AdminId = adminId
+        };
+
+        await httpClient.PostAsJsonAsync($"{ProfilesApiUrl}/TutorProfiles/Approve", profilesRequest, cancellationToken: cancellationToken);
+
+        return RedirectToPage("Index");
+    }
 }
